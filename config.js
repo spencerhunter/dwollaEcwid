c7ae595ec9bd00a66afc32dd6c5c6f10a413a2ac
Spencer Hunter <spencer.j.hunter@gmail.com>
2014-10-15 11:03:26 -0500

    Add db config variable

--- config.js
+++ config.js
@@ -4,4 +4,7 @@ module.exports = {
   , client_secret: process.env.DWOLLA_CLIENT_SECRET || null
   , pin: process.env.DWOLLA_PIN || null
   , host: process.env.HOST || 'http://localhost:3000'
+  , mysql: {
+  	uri: process.env.DATABASE_URL || 'mysql://root:root@localhost:3306/ecwidTesting',
+  }
 }
