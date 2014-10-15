edf1db195489fc449e3e2be0e29f4746a2f38274
Spencer Hunter <spencer.j.hunter@gmail.com>
2014-10-15 11:05:15 -0500

    clean up; add then catch to post payment

--- routes/index.js
+++ routes/index.js
@@ -6,89 +6,85 @@ var Dwolla = require('dwolla-node')(c.client_id, c.client_secret);
 var util = require('util');
 var crypto = require('crypto');
 var Sequelize = require('sequelize'),
-    sequelize = new Sequelize('ecwidTesting', 'root', 'root', {
-        dialect: "mysql", // or 'sqlite', 'postgres', 'mariadb'
-        port: 3306, // or 5432 (for postgres)
-    });
+  sequelize = new Sequelize(c.mysql.uri, {
+      dialect: "mysql", // or 'sqlite', 'postgres', 'mariadb'
+  });
 
 sequelize
-    .authenticate()
-    .complete(function(err) {
-        if ( !! err) {
-            console.log('Unable to connect to the database:', err)
-        } else {
-            console.log('Connection has been established successfully.')
-        }
-    });
+  .authenticate()
+  .complete(function(err) {
+    if ( !! err) {
+        console.log('Unable to connect to the database:', err)
+    } else {
+        console.log('Connection has been established successfully.')
+    }
+  });
 
 var User = sequelize.define('User', {
-    dwolla_id: Sequelize.STRING,
-    md5_hashVal: Sequelize.STRING
+  dwolla_id: Sequelize.STRING,
+  md5_hashVal: Sequelize.STRING
 });
 
 sequelize
-    .sync({})
-    .complete(function(err) {
-        if ( !! err) {
-            console.log('An error occurred while creating the table:', err)
-        } else {
-            console.log('It worked!')
-        }
-    });
+  .sync({})
+  .complete(function(err) {
+    if ( !! err) {
+        console.log('An error occurred while creating the table:', err)
+    } else {
+        console.log('It worked!')
+    }
+  });
 
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', {
-        title: 'Express'
-    });
+  res.render('index', {
+      title: 'Express'
+  });
 });
 
 router.get('/auth/dwolla', function(req, res) {
-    var redirect_uri = c.host + '/oauth_return';
-    var authUrl = Dwolla.authUrl(redirect_uri, 'AccountInfoFull');
-
-    return res.redirect(authUrl);
+  var redirect_uri = c.host + '/oauth_return';
+  var authUrl = Dwolla.authUrl(redirect_uri, 'AccountInfoFull');
+  return res.redirect(authUrl);
 });
 
 router.get('/oauth_return', function(req, res) {
-    var code = req.query.code;
-    var redirect_uri = c.host + '/oauth_return';
-    Dwolla.finishAuth(code, redirect_uri, function(error, auth) {
-        req.session.access_token = auth.access_token;
-        return res.redirect('/dashboard');
-    });
+  var code = req.query.code;
+  var redirect_uri = c.host + '/oauth_return';
+  Dwolla.finishAuth(code, redirect_uri, function(error, auth) {
+    req.session.access_token = auth.access_token;
+    return res.redirect('/dashboard');
+  });
 });
 
 router.get('/dashboard', function(req, res) {
-    if(req.session.access_token == null) {
-      res.redirect('/');
+  if(req.session.access_token == null) {
+    res.redirect('/');
+  }
+  Dwolla.setToken(req.session.access_token);
+  Dwolla.fullAccountInfo(function(err, data) {
+    if (err) {
+      res.send("There was an error finding your account " + err); // Send Error to page...redirect back to home page to Re-Authenticate.
     }
-    Dwolla.setToken(req.session.access_token);
-    Dwolla.fullAccountInfo(function(err, data) {
-        if (err) {
-            console.log(err);
+    User
+      .findOrCreate({
+        where: {
+          dwolla_id: data.Id
         }
-        console.log(data);
-        console.log(typeof data.Id);
-        User
-            .findOrCreate({
-                where: {
-                    dwolla_id: data.Id
-                }
-            })
-            .spread(function(user, created) {
-                if (created == true) {
-                    user.md5_hashVal = makeid();
-                    user.save();
-                }
-                console.log(created)
-                res.render('dashboard', {
-                    dId: user.dwolla_id,
-                    name: user.md5_hashVal
-                });
-            })
-    });
+      })
+      .spread(function(user, created) {
+        if (created == true) {
+          user.md5_hashVal = makeid();
+          user.save();
+        }
+        console.log(created)
+        res.render('dashboard', {
+          dId: user.dwolla_id,
+          name: user.md5_hashVal
+        });
+      })
+  });
 });
 
 router.get('/logout', function (req, res) {
@@ -97,46 +93,45 @@ router.get('/logout', function (req, res) {
 }); 
 
 router.post('/newCheckout', function(req, res) {
-    //var redirect_uri = 'http://localhost:3000/postPayment?x_relay_url='+ req.body.x_relay_url;
-    var ref = req.headers['referer'];
-    console.log(ref);
-    //var redirect_uri = c.host + '/postPayment/?x_relay_url=' + req.body.x_relay_url + '&referer=' + ref;
-    var redirect_uri = 'http://requestb.in/1fjp7tt1?x_relay_url=' + req.body.x_relay_url + '&referer=' + ref;
-    console.log(redirect_uri);
+    var destinationId = req.body.x_login; // grab x_login_id from POST, if no dest Id throw gateway error
+    var redirect_uri = '/postPayment?x_relay_url=' + req.body.x_relay_url + '&destinationId=' + destinationId;
+    // 'http://requestb.in/1men50c1?x_relay_url=' + req.body.x_relay_url + '&destinationId=' + destinationId;
     var purchaseOrder = {
-        destinationId: req.body.x_login,
+        destinationId: destinationId,
         total: req.body.x_amount,
         notes: req.body.x_description + ' -- Invoice number: ' + req.body.x_invoice_num
     };
-    console.log(purchaseOrder);
     var params = {
         allowFundingSources: true,
         allowGuestCheckout: true,
         additionalFundingSources: true,
         orderId: req.body.x_invoice_num,
-        callback: 'http://requestb.in/1fjp7tt1'
+        callback: 'http://requestb.in/1men50c1'
     };
-    console.log(params);
     Dwolla.createCheckout(redirect_uri, purchaseOrder, params, function(err, checkout) {
         if (err) console.log(err);
-        res.send(util.format('Checkout created.  Click to continue: <a href="%s">%s</a>', checkout.checkoutURL, checkout.checkoutURL));
+        res.redirect(checkout.checkoutURL);
     });
 });
 
 router.post('/postPayment', function(req, res) {
+    //if error is !null then display error and give the user the option to kickoff a new checkout??
+    if (req.query['error'] != null){
+      //change this to render postPayment page and display error. Also, request.post bad details to send the user back to the store.
+      res.render('There was an error with the checkout. Error description: ' + req.query['error_description']); 
+    }
     // Grab Dwolla's proposed signature
-    signature = req.body['signature'];
-    console.log(signature);
+    signature = req.query['signature'];
     // Grab Dwolla's checkout ID
-    checkout_id = req.body['checkoutId'];
+    checkout_id = req.query['checkoutId'];
     // Grab the reported total transaction amount
-    amount = req.body['amount'];
+    amount = req.query['amount'];
     // Grab the transaction Id
-    transaction_id = req.body['destinationTransaction'];
+    transaction_id = req.query['destinationTransaction'];
     //Grab the relay url
-    x_relay_url = req.body['x_relay_url'];
+    x_relay_url = req.query['x_relay_url'];
     //Grab the order ID
-    orderId = req.body['orderId'];
+    orderId = req.query['orderId'];
     // Verify the proposed signature
     did_verify = Dwolla.verifyGatewaySignature(signature, checkout_id, amount);
 
@@ -144,15 +139,13 @@ router.post('/postPayment', function(req, res) {
         console.log("Verified");
         User.find({
             where: {
-                dwolla_id: req.body['destinationId'] //change to x_login_id
+                dwolla_id: req.query['destinationId'] //change to x_login_id
             }
         }).then(function(user) {
             var hash_val = user.md5_hashVal;
             var loginId = user.dwolla_id;
             var hashstring = hash_val + loginId + transaction_id + amount;
-            console.log("hash: " + hashstring);
             var md5hashstring = crypto.createHash('md5').update(hashstring).digest('hex');
-            console.log(md5hashstring);
             request.post(
                 x_relay_url, {
                     form: {
@@ -167,17 +160,19 @@ router.post('/postPayment', function(req, res) {
                 },
                 function(error, response, body) {
                     if (!error && response.statusCode == 200) {
-                        console.log(body)
-                    }
-                    console.log("successful post");
+                        console.log(body);
+                    } //else display error of POST to Ecwid
+                    console.log("Successful post. Redirecting back to merchant.");
+                    res.send(body); //replace with page with body 
                 })
         }).catch(function(err) {
             console.log("account not found " + err);
-            res.send("<p>Account not found.</p>")
+            //If account is not found then display message to user that merchant has not configured dwolla integration properly. 
+            //Contact merchant for details.
         })
     } else {
-        console.log("failed");
-        return res.send("<p>Dwolla's signature failed to verify. You shouldn't process the order before some manual verification.</p>");
+      //
+        return res.send("<p>Dwolla's signature failed to verify. You shouldn't process the order before some manual verification. Please check you Dwolla dashboard on dwolla.com</p>");
     }
 
 });
